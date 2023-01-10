a4befea06becea707e2cd0d61589f525b42d04a2
sahil nawab <82sahil00@gmail.com>
2023-01-10 12:48:14 +0530

    operators

---
+++ src/example/Operators.java
@@ -0,0 +1,26 @@
+package example;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		int a = 99;
+		int b = 45;
+		int c = 34;
+		int sum = a+b;
+		int diff = a-b;
+		int multiply = a*b;
+		int reamanant = a%b;
+		double devide = a/b;
+		System.out.println("sum is "+ sum);
+		System.out.println("diff is "+ diff);
+		System.out.println("multiply is "+ multiply);
+		System.out.println("reamanant is "+ reamanant);
+		System.out.println("devide is "+ devide);
+		System.out.println(a+b*c);//execute lesft to right
+		System.out.println(a-b/c);//precedance given to (). after that *,/,%
+		System.out.println((a/b)*b);
+
+
+	}
+
+}
